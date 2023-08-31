e2c6aeba196aacbf005281f72248d437f3b8b75e
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-08-31 13:52:11 +0200

    refactor: animal card created

---
+++ src/components/AnimalCard.jsx
@@ -0,0 +1,18 @@
+import { SlSymbleFemale, SlSymbolMale } from "react-icons/sl";
+import { BsTrash3 } from "react-icons/bs";
+
+export default function AnimalCard({ animal, key }) {
+  const genderIcon = animal.gender === 'female'
+    ? <SlSymbleFemale className="text-md" />
+    : <SlSymbolMale className="text-md" />;
+
+  return (
+    <li key={key} className="animal-card flex justify-between w-full border border-slate-700 rounded p-2">
+      <div className="animal-info flex gap-2 items-center">
+        {genderIcon}
+        <h4 className="animal-name text-lg">{animal.name} / {animal.species}</h4>
+      </div>
+      <button><BsTrash3 /></button>
+    </li>
+  )
+}
