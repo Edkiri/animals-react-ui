b3811c428b70f7f162fccdbe3018b059a65dffa8
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-08-31 14:48:59 +0200

    feat: use AppContext to show and hide create animal form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
+import { useContext } from "react"
+import AppContext from "../contexts/AppContext"
+
 export default function AppHeader() {
+  const { isCreating, toggleCreating } = useContext(AppContext);
+
+  const buttonColor = isCreating ? 'bg-red-400' : 'bg-green-400';
+  const buttonText = isCreating ? 'Cancelar' : 'Agregar animal';
+
   return (
     <header className="flex justify-between mb-4">
-      <h1 className="text-3xl font-bold">Animals</h1>
-      <button className="bg-green-500 p-2 rounded text-md">Add animal</button>
+      <h1 className="text-3xl font-bold">Animales</h1>
+      <button onClick={toggleCreating} className={`${buttonColor} p-2 rounded text-md`}>
+        {buttonText}
+      </button>
     </header>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AnimalProvider } from './contexts/AnimalContext.jsx'
+import { AppProvider } from './contexts/AppContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AnimalProvider>
-      <App />
-    </AnimalProvider>
+    <AppProvider>
+      <AnimalProvider>
+        <App />
+      </AnimalProvider>
+    </AppProvider>
   </React.StrictMode>,
 )
