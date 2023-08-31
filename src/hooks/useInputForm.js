7b2e2350acfa2fbf44697c26a73530c887245c7c
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-08-31 15:05:29 +0200

    feat: input form hook created

---
+++ src/hooks/useInputForm.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function useInputForm(initialValue, validator) {
+  const [value, setValue] = useState(initialValue || "");
+  const [error, setError] = useState(false);
+
+  const handleChange = (event) => {
+    if (error) {
+      const isValid = validator.pattern.test(event.target.value);
+      if (isValid) {
+        setError(false);
+      }
+    }
+    setValue(event.target.value);
+  };
+
+  const handleBlur = (event) => {
+    if (!validator) return;
+    const isValid = validator.pattern.test(event.target.value);
+    if (!isValid) setError(true);
+  };
+
+  const props = {
+    value,
+    onChange: handleChange,
+    onBlur: handleBlur,
+    maxLength: 200,
+  };
+
+  if (validator) {
+    props.errorMessage = validator.errorMessage;
+    props.error = error;
+  }
+
+  return props;
+}
