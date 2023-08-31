829ab6cb6e2b44dce39aad1ce7c33e5ad8908456
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-08-31 16:43:54 +0200

    feat: read API_URL from .env file

--- src/hooks/useAnimals.js
+++ src/hooks/useAnimals.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { API_URL } from "../constants";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function useAnimals() {
   const [animals, setAnimals] = useState([]);
